b1e99fe55bcc1ded802420737ad8285fc4a1e860
xhk <helloxhk@163.com>
2022-03-18 23:44:03 +0800

    hot-fix 第一次提交

--- src/main/java/com/xhk/git/GitTest.java
+++ src/main/java/com/xhk/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("再来一个");
+        System.out.println("再来一个2");
     }
 }
