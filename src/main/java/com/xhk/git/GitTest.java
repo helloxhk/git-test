4a208d251e076e39a4297ccc4dd22ec0e4d59066
xhk <helloxhk@163.com>
2022-03-19 00:05:40 +0800

    push之前

--- src/main/java/com/xhk/git/GitTest.java
+++ src/main/java/com/xhk/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("再来一个2");
         System.out.println("再来一个2，hot-fix");
         System.out.println("再来一个，master");
+        System.out.println("push一下");
     }
 }
